ddd7286c0482e3af2f4b0e7129ddd7f431784791
ThisIsSinclair <fenriskeeper@gmail.com>
2014-03-22 23:39:50 -0500

    Second Commit
    Finished the primary requirements for plugin.yml and the Summoner main
    class.

--- Summoner/src/io/github/ThisIsSinclair/Summoner/Summoner.java
+++ Summoner/src/io/github/ThisIsSinclair/Summoner/Summoner.java
@@ -1,5 +1,14 @@
 package io.github.ThisIsSinclair.Summoner;
 
-public class Summoner {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public final class Summoner extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+	}
+	
+	@Override
+	public void onDisable() {
+	}
 }
