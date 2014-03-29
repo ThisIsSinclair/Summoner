df70bd22f28f6b20d9ff34edb6db6edddc9e75b1
ThisIsSinclair <fenriskeeper@gmail.com>
2014-03-29 17:21:16 -0500

    Initial Commit
    Contains the initial Maven Project, Package, Class, and plugin.yml

---
+++ src/main/java/io/github/ThisIsSinclair/Summoner/Summoner.java
@@ -0,0 +1,17 @@
+package io.github.ThisIsSinclair.Summoner;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Summoner extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+
+}
